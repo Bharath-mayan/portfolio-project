141114d22825f735a7b05dc5fd002333dae4c2cb
Bharath-mayan <bharathmayan4454@gmail.com>
2025-01-26 09:38:52 +0530

    Initial commit

---
+++ src/app/base/home/home.component.ts
@@ -0,0 +1,64 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+
+  menuBarImage: string = '/assets/images/menu-bar.png';
+  showMenu: boolean = false;
+  activeBlock: any = '';
+
+  @ViewChild('home', { static: false }) goToHome!: ElementRef;
+  @ViewChild('about', { static: false }) goToAbout!: ElementRef;
+  @ViewChild('projects', { static: false }) goToProject!: ElementRef;
+  @ViewChild('skills', { static: false }) goToSkill!: ElementRef;
+  @ViewChild('services', { static: false }) goToService!: ElementRef;
+  @ViewChild('contacts', { static: false }) goToContact!: ElementRef;
+
+
+
+  ngOnInit(): void {
+
+  }
+
+  visibleMenu() {
+    const navList: any = document.getElementById("nav");
+    navList.style.display = "block";
+
+    this.showMenu = true;
+    this.menuBarImage = this.menuBarImage === '/assets/images/menu-bar.png' ? '/assets/images/close_bar.png' : '/assets/images/menu-bar.png';
+    if (this.menuBarImage != '/assets/images/close_bar.png')
+      navList.style.display = "none";
+    this.showMenu = !this.showMenu;
+  }
+
+  showBlock(barName: any) {
+    this.activeBlock = barName;
+
+    if (this.activeBlock === 'Home') {
+      this.scrollToSection(this.goToHome);
+    } else if (this.activeBlock === 'About') {
+      this.scrollToSection(this.goToAbout);
+    } else if (this.activeBlock === 'Projects') {
+      this.scrollToSection(this.goToProject);
+    } else if (this.activeBlock === 'Services') {
+      this.scrollToSection(this.goToService);
+    } else if (this.activeBlock === 'Skills') {
+      this.scrollToSection(this.goToSkill);
+    } else if (this.activeBlock === 'Contacts') {
+      this.scrollToSection(this.goToContact);
+    }
+  }
+
+  private scrollToSection(section: ElementRef) {
+    section.nativeElement.scrollIntoView({ behavior: 'smooth' });
+  }
+
+
+
+}
+
